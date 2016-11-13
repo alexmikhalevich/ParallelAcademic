1a957412bf0d483e23db1e21965e4bfd1cb7a44d
maked0n <alex.mikhalevich@gmail.com>
2016-11-13 23:04:01 +0300

    Small refactoring

--- life_game/cfield.cpp
+++ life_game/cfield.cpp
@@ -1,14 +1,11 @@
 #include "cfield.h"
 
 CField::CField(const std::string& input) {
+	m_cur_state = 0;
 	std::ifstream in(input);
 	size_t sizex, sizey;
 	in >> sizex >> sizey;
-	m_state.resize(sizex);
-	for(size_t i = 0; i < sizex; ++i) {
-		m_state[i].resize(sizey);
-		for(size_t j = 0; j < sizey; j++) m_state[i][j] = false;
-	}
+	_init_state(m_state, sizex, sizey);
 	while(!in.eof()) {
 		size_t x, y;
 		in >> x >> y;
@@ -16,10 +13,27 @@ CField::CField(const std::string& input) {
 	}
 }
 
-void write_state(const std::string& output) const {
+void CField::_init_state(State& state, size_t xsize, size_t ysize) {
+	for(size_t i = 0; i < sizex; ++i) {
+		state[i].resize(sizey);
+		for(size_t j = 0; j < sizey; j++) state[i][j] = false;
+	}
+}
+
+void CField::write_state(const std::string& output) const {
 	std::ofstream out(output);
 	out << m_state.size() << " " << m_state[0].size() << std::endl;
 	for(size_t i = 0; i < m_state.size(); ++i) 
 		for(size_t j = 0; j < m_state[0].size(); ++j) 
 			if(m_state[i][j]) out << i << " " << j << std::endl;
+}
+
+void CField::step() {
+	State next_state;
+	_init_state(next_state);
+	for(size_t i = 0; i < m_state.size(); ++i) {
+		for(size_t j = 0; j < m_state[0].size(); ++j) {
+
+		}
+	}
 }
--- life_game/cfield.h
+++ life_game/cfield.h
@@ -2,9 +2,12 @@
 #include <fstream>
 #include <string>
 
+typedef std::vector<std::vector<bool>> State;
+
 class CField {
 	private:
-		std::vector<std::vector<bool>> m_state;
+		State m_state;
+		void _init_state(State& state, size_t xsize, size_t ysize);
 	public:
 		CField(const std::string input);
 		void step();
