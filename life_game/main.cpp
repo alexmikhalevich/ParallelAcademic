2318755119fc24ae8f88f208d5346a758dc34df1
maked0n <alex.mikhalevich@gmail.com>
2016-11-13 19:34:58 +0000

    Main functionality implemented

--- life_game/main.cpp
+++ life_game/main.cpp
@@ -1,1 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include "cfield.h"
 
+int main(int argc, char** argv) {
+	size_t time = 0;
+	if(argc < 3) {
+		std::cout << "Missing arguments!" << std::endl;
+		return -1;
+	}
+	for(int i = 1; i < argc; ++i) {
+		if(argv[i] == "--time" || argv[i] == "-t") time = atoi(argv[++i]);
+	}
+	CField* field = new CField("state.dat");
+	while(time != 0) {
+		field->step();
+		--time;
+	}
+	field->write_state("life.dat");
+	delete field;
+
+	return 0;
+}
