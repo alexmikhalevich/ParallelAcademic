c6508769787e2eff7e23dc040e985d415aeba779
maked0n <alex.mikhalevich@gmail.com>
2016-12-26 13:52:54 +0000

    Time calculating staff added

--- life_game/main.cpp
+++ life_game/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <fstream>
 #include "cfield.h"
 
 #ifdef MULTITHREAD
@@ -9,13 +10,17 @@
 
 int main(int argc, char** argv) {
 	size_t time = 0;
+	size_t etalon = 0;
 	if(argc < 3) {
 		std::cout << "Missing arguments!" << std::endl;
 		return -1;
 	}
 	for(int i = 1; i < argc; ++i) {
-		if(strcmp(argv[i], "--time") == 0 || strcmp(argv[i], "-t") == 0) time = atoi(argv[++i]);
+		if(strcmp(argv[i], "--time") == 0 || strcmp(argv[i], "-t") == 0)
+			time = atoi(argv[++i]);
 	}
+	std::ofstream out_time("out.dat");
+	std::ofstream out_eff("eff.dat");
 
 #ifdef MULTITHREAD
 	for(int i = 1; i < MAX_THREADS; ++i) {
@@ -32,7 +37,10 @@ int main(int argc, char** argv) {
 		delete field;
 #ifdef MULTITHREAD
 		end = omp_get_wtime();
+		if(i == 1) etalon = end - begin;
 		std::cout << "Num of threads: " << i << "; time: " << end - begin << std::endl;
+		out_time << i << ":" << (end - begin) / etalon << std::endl;
+		out_eff << i << ":" << (end - begin) / (etalon * i) << std::endl;
 	}
 #endif
 	return 0;
